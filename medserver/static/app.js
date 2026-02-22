25b4e776f6916e8ab3ece760049fb9f462723e0a
yyhx8 <yahyax9x@gmail.com>
2026-02-23 04:39:33 +0700

    trm

--- medserver/static/app.js
+++ medserver/static/app.js
@@ -710,21 +710,20 @@
 
         const hwContainer = document.getElementById('hwStatsContainer');
         if (hwContainer) {
-            if (data.gpu_name) {
-                hwContainer.classList.remove('hidden');
-                if (els.gpuName) els.gpuName.textContent = data.gpu_name;
-                
-                if (els.gpuVram) {
-                    if (data.gpu_vram_used_gb !== undefined && data.gpu_vram_total_gb !== undefined) {
-                        els.gpuVram.textContent = `${data.gpu_vram_used_gb} / ${data.gpu_vram_total_gb} GB`;
-                    } else if (data.gpu_vram_gb) {
-                        els.gpuVram.textContent = `${data.gpu_vram_gb} GB`;
-                    } else {
-                        els.gpuVram.textContent = '--';
-                    }
+            hwContainer.classList.remove('hidden');
+
+            if (els.gpuName) {
+                els.gpuName.textContent = data.gpu_name || 'Not shared';
+            }
+
+            if (els.gpuVram) {
+                if (data.gpu_vram_used_gb !== undefined && data.gpu_vram_total_gb !== undefined) {
+                    els.gpuVram.textContent = `${data.gpu_vram_used_gb} / ${data.gpu_vram_total_gb} GB`;
+                } else if (data.gpu_vram_gb) {
+                    els.gpuVram.textContent = `${data.gpu_vram_gb} GB`;
+                } else {
+                    els.gpuVram.textContent = 'Not shared';
                 }
-            } else {
-                hwContainer.classList.add('hidden');
             }
         }
 
